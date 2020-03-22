989656255ae226a0a0d43ffb0aaa0b919a12659c
Philippe VILLET <pvillet@hotmail.com>
2020-03-22 16:25:01 +0100

    Update Turtle_tmp.ino
    Premiere version fonctionnelle. Youpi

--- Turtle_tmp/Turtle_tmp.ino
+++ Turtle_tmp/Turtle_tmp.ino
@@ -16,23 +16,29 @@ DallasTemperature sensors(&oneWire);
 LCD16x2 lcd;
 
 int buttons;
+char tbuffer[7];
+char line[7];
+float Current_Temp = 20.00;
+float Max_Temp = -50.00;
+float Min_Temp = 100.00;
+
 
 void setup(){
   Wire.begin();
 
   lcd.lcdClear();
   
-  lcd.lcdGoToXY(2,1);
-  lcd.lcdWrite("BUT1:");
-  
-  lcd.lcdGoToXY(10,1);
-  lcd.lcdWrite("BUT2:");
-  
-  lcd.lcdGoToXY(2,2);
-  lcd.lcdWrite("BUT3:");
-  
-  lcd.lcdGoToXY(10,2);
-  lcd.lcdWrite("BUT4:");
+//  lcd.lcdGoToXY(2,1);
+//  lcd.lcdWrite("BUT1:");
+//  
+//  lcd.lcdGoToXY(10,1);
+//  lcd.lcdWrite("BUT2:");
+//  
+//  lcd.lcdGoToXY(2,2);
+//  lcd.lcdWrite("BUT3:");
+//  
+//  lcd.lcdGoToXY(10,2);
+//  lcd.lcdWrite("BUT4:");
 
   // start serial port
   Serial.begin(9600);
@@ -42,32 +48,41 @@ void setup(){
   sensors.begin();  
 }
 void loop(){
-  
+
+for (int l = 0; l <100; l++)  {
   buttons = lcd.readButtons();
+  if( buttons != 15) {
+     Serial.println("Appui detecte");    
+     Max_Temp = -50.00;
+     Min_Temp = 100.00;
+  }
+   delay(10);
+}
+
+  //buttons = lcd.readButtons();
+//  Serial.println("button: ");
+//  Serial.println(buttons);
   
-  lcd.lcdGoToXY(7,1);
-  if(buttons & 0x01)  
-    lcd.lcdWrite("0");
-  else
-    lcd.lcdWrite("1");
-    
-  lcd.lcdGoToXY(15,1);
-  if(buttons & 0x02)  
-    lcd.lcdWrite("0");
-  else
-    lcd.lcdWrite("1");
-    
-  lcd.lcdGoToXY(7,2);
-  if(buttons & 0x04)  
-    lcd.lcdWrite("0");
-  else
-    lcd.lcdWrite("1");
-    
-  lcd.lcdGoToXY(15,2);
-  if(buttons & 0x08)  
-    lcd.lcdWrite("0");
-  else
-    lcd.lcdWrite("1");
+//  lcd.lcdGoToXY(7,1);
+     
+//    
+//  lcd.lcdGoToXY(15,1);
+//  if(buttons & 0x02)  
+//    lcd.lcdWrite("0");
+//  else
+//    lcd.lcdWrite("1");
+//    
+//  lcd.lcdGoToXY(7,2);
+//  if(buttons & 0x04)  
+//    lcd.lcdWrite("0");
+//  else
+//    lcd.lcdWrite("1");
+//    
+//  lcd.lcdGoToXY(15,2);
+//  if(buttons & 0x08)  
+//    lcd.lcdWrite("0");
+//  else
+//    lcd.lcdWrite("1");
 
   // call sensors.requestTemperatures() to issue a global temperature 
   // request to all devices on the bus
@@ -77,8 +92,32 @@ void loop(){
   // After we got the temperatures, we can print them here.
   // We use the function ByIndex, and as an example get the temperature from the first sensor only.
   Serial.print("Temperature for the device 1 (index 0) is: ");
-  Serial.println(sensors.getTempCByIndex(0));  
-    
-  delay(100);
+  Current_Temp = sensors.getTempCByIndex(0);
+  Serial.println(Current_Temp);  
+
+  if (Current_Temp > Max_Temp)
+     Max_Temp = Current_Temp;
+     
+  if (Current_Temp < Min_Temp)
+     Min_Temp = Current_Temp;
+
+/* Affichage premiere ligne */
+  dtostrf(Current_Temp,4,2,tbuffer);
+  sprintf(line, "%s C", tbuffer);
+  lcd.lcdGoToXY(5,1);
+  lcd.lcdWrite(line);
+
+/* Affichage deuxieme ligne */
+  dtostrf(Min_Temp,4,2,tbuffer);
+  sprintf(line, "%s C", tbuffer);
+  lcd.lcdGoToXY(1,2);
+  lcd.lcdWrite(line);
+
+  dtostrf(Max_Temp,4,2,tbuffer);
+  sprintf(line, "%s C", tbuffer);
+  lcd.lcdGoToXY(9,2);
+  lcd.lcdWrite(line);
+  
+//  delay(1000);
 
 }
